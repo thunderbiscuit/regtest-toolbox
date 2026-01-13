abc730725abd5de62a42051bae8691b2fbc71ddf
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-01-13 13:36:31 -0500

    Add base library template

---
+++ lib/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    alias(libs.plugins.jvm)
+
+    // Apply the java-library plugin for API and implementation separation.
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {}
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use Kotlin Test test framework
+            useKotlinTest("1.9.20")
+        }
+    }
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "regtest-kit"
+include("lib")
