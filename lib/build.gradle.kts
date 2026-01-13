2415455bddbe7478397a6a3396c8f5a126ffb3ac
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-01-13 13:40:48 -0500

    Clean up build script

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,9 +1,5 @@
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    alias(libs.plugins.jvm)
-
-    // Apply the java-library plugin for API and implementation separation.
-    `java-library`
+    id("org.jetbrains.kotlin.jvm") version "2.3.0"
 }
 
 repositories {
