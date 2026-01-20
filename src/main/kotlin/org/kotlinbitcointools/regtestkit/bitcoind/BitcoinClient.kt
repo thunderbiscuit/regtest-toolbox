995e04e786fd783b9eba72f6db52d38ed8dd0b8f
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-01-20 12:43:02 -0500

    Add Apache 2.0 license

--- src/main/kotlin/org/kotlinbitcointools/regtestkit/bitcoind/BitcoinClient.kt
+++ src/main/kotlin/org/kotlinbitcointools/regtestkit/bitcoind/BitcoinClient.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2026 Kotlin Bitcoin Tools contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE.txt file.
+ */
+
 package org.kotlinbitcointools.regtestkit.bitcoind
 
 import io.ktor.client.HttpClient
